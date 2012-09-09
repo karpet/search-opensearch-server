059dc301f2b95b146112c0a9ae9832607a27436d
Peter Karman <karpet@peknet.com>
2012-09-08 22:06:15 -0500

    add new() method since R::O::CAF used to supply it

--- lib/Search/OpenSearch/Result.pm
+++ lib/Search/OpenSearch/Result.pm
@@ -18,6 +18,21 @@ use overload
 
 our $VERSION = '0.17_01';
 
+sub new {
+    my $proto = shift;
+    my $class = ref $proto || $proto;
+
+    my $self;
+    if ( @_ == 1 && ref $_[0] eq 'HASH' ) {
+        $self = bless { %{ $_[0] } }, $class;
+    }
+    else {
+        $self = bless {@_}, $class;
+    }
+
+    $self;
+}
+
 sub stringify {
     my $self = shift;
 
